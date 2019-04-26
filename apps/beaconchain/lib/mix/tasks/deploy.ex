b3fe2c57e0aa4f00128c1cd03e2cb62b36910c67
Tim Jester-Pfadt <tim.jester-pfadt@merckgroup.com>
2019-04-26 10:06:14 +0200

    Add script to deploy deposit contract.

---
+++ apps/beaconchain/lib/mix/tasks/deploy.ex
@@ -0,0 +1,37 @@
+defmodule Mix.Tasks.Deploy do
+  use Mix.Task
+
+  @moduledoc """
+  Starts local test blockchain with validator deposit contract deployed.
+  CHAIN_START_FULL_DEPOSIT_THRESHOLD is set to 4 to start a Eth2Genesis log.
+  MIN_DEPOSIT is 1 ETH and MAX_DEPOSIT is 32 ETH.
+  """
+
+  require Ethereumex
+  require Logger
+
+  @shortdoc "Deploys the deposit contract."
+  def run(_argv) do
+    Logger.info("Deploying validator deposit contract.")
+    {:ok, _started} = Application.ensure_all_started(:ethereumex)
+    Logger.info("Started Ethereumex Application.")
+    accounts = ExW3.accounts()
+    Logger.info("Got list of accounts from RPC.")
+    ExW3.Contract.start_link()
+    Logger.info("Started contract manager.")
+    abi = ExW3.load_abi("apps/beaconchain/lib/mix/tasks/DepositContract.abi")
+    Logger.info("Loaded contract ABI.")
+    a = ExW3.Contract.register(:DepositContract, abi)
+    Logger.info("Contract registered.")
+    IO.inspect(a)
+
+    {:ok, address, tx_hash} =
+      ExW3.Contract.deploy(:DepositContract,
+        bin: ExW3.load_bin("apps/beaconchain/lib/mix/tasks/DepositContract.bin"),
+        options: %{gas: 300_000, from: Enum.at(accounts, 0)}
+      )
+
+    ExW3.Contract.at(:DepositContract, address)
+    Logger.info("Contract deployed at: #{address} with tx #{tx_hash}")
+  end
+end
