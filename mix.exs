ebeca70d354b4435031fc145c743454ba0ca1b26
Tim Jester-Pfadt <tim.jester-pfadt@merckgroup.com>
2019-04-27 17:02:10 +0200

    Updated dependencies

--- mix.exs
+++ mix.exs
@@ -4,6 +4,13 @@ defmodule Panacea.MixProject do
   def project do
     [
       apps_path: "apps",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -15,6 +22,12 @@ defmodule Panacea.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:distillery, "~> 2.0"},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:benchee, "~> 0.9", only: :dev}
+    ]
   end
 end
