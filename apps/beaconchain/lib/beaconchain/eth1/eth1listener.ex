439c9e7892fdf5a0b8adda3893c297c8b063f44e
Tim Jester-Pfadt <tim.jester-pfadt@merckgroup.com>
2019-06-01 19:37:05 +0200

    Preliminary ETH1 deposit contract listener

--- apps/beaconchain/lib/beaconchain/eth1/eth1listener.ex
+++ apps/beaconchain/lib/beaconchain/eth1/eth1listener.ex
@@ -5,9 +5,20 @@ defmodule Beaconchain.Eth1Listener do
   will be listed to for tracking of new validators.
   """
   use GenServer
+  require Logger
 
   def init(init_arg) do
+    Logger.info("Start listening for deposit logs...")
     ExW3.Contract.start_link()
+    abi = ExW3.load_abi("apps/beaconchain/lib/mix/tasks/DepositContract.abi")
+    ExW3.Contract.register(:DepositContract, abi: abi)
+
+    ExW3.Contract.at(
+      :DepositContract,
+      Application.get_env(:beaconchain, :deposit_contract_address)
+    )
+
+    # {:ok, filter_id} = ExW3.Contract.filter(:DepositContract, "Deposit")
     listen_for_event()
     {:ok, init_arg}
   end
@@ -16,7 +27,9 @@ defmodule Beaconchain.Eth1Listener do
     GenServer.start_link(__MODULE__, opts)
   end
 
-  def listen_for_event do
+  def listen_for_event() do
+    {:ok, changes} = ExW3.Contract.call(:DepositContract, :get)
+    Logger.info(changes)
     :timer.sleep(1000)
     # Recurse
     listen_for_event()
