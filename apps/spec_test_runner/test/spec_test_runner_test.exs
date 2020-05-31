36cf6be309f21c0017a62842e22b3c1761e2ee21
Tim Jester-Pfadt <tim.jester-pfadt@merckgroup.com>
2020-05-31 21:56:43 +0200

    Move to spec-test-runner

--- apps/spec_test_runner/test/spec_test_runner_test.exs
+++ apps/spec_test_runner/test/spec_test_runner_test.exs
@@ -1,8 +1,12 @@
 defmodule SpecTestRunnerTest do
   use ExUnit.Case
-  doctest SpecTestRunner
+
+  File.cd("../../eth2.0-spec-tests/general/phase0/ssz-generic")
+  @test_vector_path
+  File.ls(@test_vector_path)
+    |> Enum.each(&IO.puts/1)
 
   test "greets the world" do
-    assert SpecTestRunner.hello() == :world
+    assert true
   end
 end
