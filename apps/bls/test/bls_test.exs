c9ea979627f0b5b94af8a8272e96c195fa5b8c09
Tim Jester-Pfadt <tim.jester-pfadt@merckgroup.com>
2020-05-31 13:08:20 +0200

    Remove initial subapps

--- apps/bls/test/bls_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule BlsTest do
-  use ExUnit.Case
-  doctest Bls
-
-  test "greets the world" do
-    assert Bls.hello() == :world
-  end
-end
--- apps/szz/test/szz_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SzzTest do
-  use ExUnit.Case
-  doctest Szz
-
-  test "greets the world" do
-    assert Szz.hello() == :world
-  end
-end
